58b3f5dc493b5b0b77b1011e520536f72d3d66cf
Cromano31415 <cromano31415@gmail.com>
2018-05-23 22:09:43 -0500

    done

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,7 +1,9 @@
 require 'httparty'
+require 'json'
 
 class Kele
 	include HTTParty
+	include JSON
 
 
   	base_uri 'https://www.bloc.io/api/v1'
@@ -12,5 +14,9 @@ class Kele
 		@auth_token = response["auth_token"]
 	end
 
+	def get_me
+		response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+		@user = JSON.parse(response.body)
+	end 
 
 end 
